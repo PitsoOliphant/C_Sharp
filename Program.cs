bcca98323d10a83888076e6ae3fc90522261d38f
Pitso Oliphat <111675282+PitsoOliphant@users.noreply.github.com>
2023-05-01 14:24:06 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,160 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// See https://aka.ms/new-console-template for more information
 
-namespace PRG161_Test1PitsoOliphant_578437
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int AvailableBalance = 600;
-            string UserName = "Pitso";
-            int Pin = 12345;
-            int Transaction = 0;
-            int iterator1 = 1;
-            int Amount = 0;
-            string[] Rate = new string[] { "bad", "good", "Excellent" };
-   
-           
-          
-            
-            for (int i = 1; i <= 3; i++)
-            {
-                Console.WriteLine("Please enter user name");
-                string UserNameInput=Console.ReadLine();
-                Console.WriteLine("Please enter your pin");
-                int UserPinInut = Convert.ToInt16(Console.ReadLine());
-                if (UserNameInput != UserName && UserPinInut != Pin)
-                {
-                    Console.WriteLine("Incorrect pin or user name");
-                    if(i==3)
-                    {
-                        Console.WriteLine("You have exhausted your attempts.");
-
-                    }
-                }
-                else
-                {
-                    i = 3;
-                    while (iterator1 > 0)
-                    {
-                        Console.WriteLine("Please select the following transaction: \r\n 1) Send Money\r\n 2) Buy Airtime\r\n 3) View Balance\r\n 4) Exit");
-                        Transaction = Convert.ToInt16(Console.ReadLine());
-                        if (Transaction == 1)
-                        {
-
-
-                            Console.WriteLine("Enter the amount you would like to send");
-                            Amount = Convert.ToInt16(Console.ReadLine());
-                           
-                            if (Amount > AvailableBalance)
-                            {
-                                Console.WriteLine("Insufficient amount! Please try again");
-                            }
-                            else
-                            {
-
-                                AvailableBalance = AvailableBalance - Amount;
-                                Console.WriteLine("You have successfully sent: R" + Amount);
-                             
-
-                            }
+//Question_1:Write code that attempts to add an int and a string and save the result in a int
+int Num1 = 10;
+string Pitso="10";
+int Result = Num1 + Convert.ToInt16(Pitso);
+Console.WriteLine(Result);
+//Question_2:Write code that attempts to add an int and a string and save the result in a string.Using type casting
+int Num2 = 5;
+string str2 = "five";
+string str3 = Convert.ToString(Num2) + ": "+str2;
+Console.WriteLine(str3);
+/*Question_3:Write program that stores an integer value.Print the integer value. 
+ * Convert the value to a decimal using type casting. Print the decimal value*/
+int Num3 = 54;
+double D1;
+Console.WriteLine(Num3);
+D1 = (double)Num3;
+Console.WriteLine(D1);
+/*Question_4: Write program that stores a decimal value. Print the decimal value.
+ * Convert the value into a integer value using type casting. Print the integer value*/
+double D2 = 54.6597;
+int Num4;
+Num4 = (int)D2;
+Console.WriteLine("This is the double value:"+D2);
+Console.WriteLine("This is the converted integer value: " + Num4);
+/*Question_5: Create a variable that stores a character, 'A' using implicit conversion*/
+char chr = 'A';
+int Num5 = chr;
+long LongNum1 = chr;
+float FloatNum1 = chr;  
+double DoubleNum1 = chr;
+Console.WriteLine("From char to int: "+Num5);
+Console.WriteLine("From char to long: " + LongNum1);
+Console.WriteLine("From char to float: " + FloatNum1);
+Console.WriteLine("From char to doubl: " + DoubleNum1);
 
 
 
 
-                        }
-
-                        if (Transaction == 2)
-                        {
-                            for (int b = 1; b > 0; b++)
-                            {
-                                Console.WriteLine("Enter the amount of airtime you would like to purchase");
-                                Amount = Convert.ToInt16(Console.ReadLine());
-                                if (Amount > AvailableBalance)
-                                {
-                                    Console.WriteLine("Insufficient amount! Please try again");
-                                }
-                                else
-                                {
-                                    AvailableBalance = AvailableBalance - Amount;
-                                    Console.WriteLine("Transaction was successfull");
-                                    break;
-
-                                }
-                            }
-
-                         }
-
-                        if (Transaction == 3)
-                         {
-                        Console.WriteLine("You have R" + AvailableBalance + " remaining in your account");
-                         }
-
-                        if (Transaction == 4)
-                         {
-
-                            for (int c = 0; i < 4; c++)
-                            {
-
-                                Console.WriteLine("Please rate our service. 1 to 3 \n1 being poor and 3 being excellent");
-                                int service = Convert.ToInt16(Console.ReadLine());
-                                if (service == 1)
-                                {
-                                    Console.WriteLine(Rate[0]);
-                                    Console.WriteLine("===============================================================");
-                                    Console.WriteLine("Thank you!");
-                                    Console.WriteLine("===============================================================");
-                                    break;
-                                }
-                                else if (service == 2)
-                                {
-                                    Console.WriteLine(Rate[1]);
-                                    Console.WriteLine("===============================================================");
-                                    Console.WriteLine("Thank you!");
-                                    Console.WriteLine("===============================================================");
-                                    break;
-                                }
-                                else if (service == 3)
-                                {
-                                    Console.WriteLine(Rate[2]);
-                                    Console.WriteLine("===============================================================");
-                                    Console.WriteLine("Thank you!");
-                                    Console.WriteLine("===============================================================");
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Invalid selection. Please try again");
-                                    c--;
-                                }
-                            }
-
-
-                            break;
-                         }
-                        iterator1++;
-                    }
-
-
-
-
-
-                }
-
-            }
-
-
-
-
-
-             Console.WriteLine("Press any key to teminate");
-            
-            
-              Console.ReadKey();
-        }
-    }
-}
+Console.ReadLine();
